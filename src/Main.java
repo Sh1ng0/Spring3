232b5cb3c04a733eebab20d67e8f4a0a49ce2f92
Josiko <reye31416@gmail.com>
2024-10-24 12:41:34 +0200

    Super Main

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
-import singleton.SingletonUndo;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        /* Digievolucionarlo a SuperMain */
 
-        SingletonUndo undoManager = SingletonUndo.getInstance();
-
-        System.out.println("Añadir comandos:");
-        undoManager.addCommand();
-        undoManager.addCommand();
-        undoManager.addCommand();
-
-        System.out.println("Historial:");
-        undoManager.showCommads();
-
-        System.out.println("Borrar comandos:");
-        undoManager.deleteCommand();
-
-        System.out.println("Historial:");
-        undoManager.showCommads();
     }
 
 }
